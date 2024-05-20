9d75fd820f9b958e7059c66b7e9b9f65a355c093
WalterCorsini <corsini.walter@gmail.com>
2024-05-20 18:46:43 +0200

    fix some bugs

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
 $long_text = $_GET["text"];
 $bad_word = $_GET["bad-word"];
-$count = 0;
+$replace = "***";
 ?>
 
 <!DOCTYPE html>
@@ -18,32 +18,34 @@ $count = 0;
 <body>
     <div class="container">
         <section>
-            <h2>il paragrafo inserito</h2>
+            <h2>il paragrafo inserito è: </h2>
             <?php
-            echo "$long_text";
+                echo "$long_text";
             ?>
         </section>
 
         <section>
-            <h2>è lungo:</h2>
+            <h2>la sua lunghezza è di:</h2>
             <div>
                 <?php
-                echo strlen("$long_text");
+                    echo strlen("$long_text");
                 ?>
                 <span>caratteri</span>
             </div>
         </section>
 
         <section>
-            <h2> dal paragrafo abbiamo nascosto la parola</h2>
             <?php
-            $bad_word = strtolower("$bad_word");
-            //  str_replace per sostituire una parola inclusa in un testo
-            //  str_ireplace non è key sensitive e quindi non c'è bisogno di trasformare tutto in minuscolo per cercare la corrispondenza
-            // $long_text = str_ireplace("$bad_word", "***", $long_text);
-            $long_text = preg_replace("/\b{$bad_word}\b/", "***", $long_text);
-            echo "$long_text";
+                $bad_word = strtolower("$bad_word");
+                $long_text = strtolower("$long_text");
+                $long_text = preg_replace("/\b{$bad_word}\b/","$replace","$long_text");
             ?>
+            <h2> il paragrafo con la censura è:</h2>
+
+            <?php 
+                echo "$long_text";
+            ?>
+
         </section>
     </div>
 
